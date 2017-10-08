54b195869744b5e85b00a5c94511a8ff2e8b8bcb
walterlv <xblero@gmail.com>
2017-10-08 13:22:38 +0800

    Start game loop code.

--- src/ReversiGame/ReversiGame/MainPage.xaml.cs
+++ src/ReversiGame/ReversiGame/MainPage.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
-
 namespace Walterlv.Gaming.Reversi
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            ContentFrame.Navigate(typeof(MenuPage));
         }
     }
 }
---
+++ src/ReversiGame/ReversiGame/View/MenuPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI.Xaml.Controls;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+namespace Walterlv.Gaming.Reversi
+{
+    public sealed partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CanvasAnimatedControl_CreateResources(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs args)
+        {
+
+        }
+
+        private void CanvasAnimatedControl_Update(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
+        {
+            
+        }
+
+        private void CanvasAnimatedControl_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        {
+            
+        }
+    }
+}
